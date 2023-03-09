e7a2dc6231e059dd7b84d0c0faf158bd38144534
Duzhiev <magomed.duzhiev@gmail.com>
2023-03-09 19:12:45 +0300

    начало

---
+++ Back/index.js
@@ -0,0 +1,27 @@
+const mongoose = require("mongoose");
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+const morgan = require("morgan");
+
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+app.use(morgan("dev"));
+
+const { PORT, MONGO_SERVER } = process.env;
+
+const connectAndStartServer = async () => {
+  try {
+    await mongoose.connect(MONGO_SERVER);
+
+    app.listen(PORT, () => {
+      console.log(`Успешно соеденились. ПОРТ ${PORT}`);
+    });
+  } catch (e) {
+    console.log(`Ошибка при подключении: ${e.toString()}`);
+  }
+};
+
+connectAndStartServer();
